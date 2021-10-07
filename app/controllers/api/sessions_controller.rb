632b813dd3ac37071c94cc5fac14f60ce76c0339
tkla <kennyla95@gmail.com>
2021-10-07 11:41:35 -0700

    Disabled admin account login for all users

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::SessionsController < ApplicationController
    def create
       # Find user by credentials
       username = ''  
+      # If logging in by email
       if params[:user][:email] 
          username = User.find_by(email: params[:user][:email])
          if !username 
@@ -12,6 +13,12 @@ class Api::SessionsController < ApplicationController
          username = params[:user][:username]
       end
       
+      # Disable admin login. 
+      if username == 'Admin'
+         return render json: ['You do not have access to Admin account'], status: 401
+      end
+      
+      # Get username from emails then use as credentials
       @user = User.find_by_credentials(username, params[:user][:password])
       if @user.nil?
          render json: ['Login or password is invalid.'], status: 401
